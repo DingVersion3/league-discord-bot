8d63cdfb58dd1fe545f4ff2240a50326cf2cf0c2
DingVersion3 <jakewilk10@gmail.com>
2026-07-28 16:56:13 -0700

    .sh fix

--- run_bot.sh
+++ run_bot.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 source venv/bin/activate
 while true; do
-    python -m leaguebot.bot
+    python -u -m leaguebot.bot
     echo "Bot exited (code $?). Restarting in 5 seconds..."
     sleep 5
 done
